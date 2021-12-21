9c9f2a5edf2b131c17a6ede242bbf7868bd33586
Brandon Phillips <bphillips09@me.com>
2021-12-21 15:27:51 -0600

    Updates for UI, scoring, and feature-completion

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,32 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using TMPro;
+using UnityEngine.UI;
+using Array = System.Array;
+using Enum = System.Enum;
+using System.Text.RegularExpressions;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
-    private List<Card> deck;
-    private List<Card> currentHand;
     [SerializeField] private UICard[] gameCards;
     [SerializeField] private Sprite[] suitSprite;
     [SerializeField] private Sprite[] cardTypeSprite;
     [SerializeField] private Sprite[] deckSprites;
+    [SerializeField] private Image[] betMultiplierImages;
+    [SerializeField] private Color betSelectedColor;
+    [SerializeField] private Color betInactiveColor;
+    [SerializeField] private TextMeshProUGUI betText;
+    [SerializeField] private TextMeshProUGUI winText;
+    [SerializeField] private TextMeshProUGUI creditsText;
+    [SerializeField] private GameObject gameOverWindow;
+    [SerializeField] private GameObject resetWindow;
+    [SerializeField] private Button betOneButton;
+    [SerializeField] private Button betMaxButton;
+    private bool resetBet = false;
+    private bool firstLaunch = true;
+    private List<Card> deck;
+    private List<Card> currentHand;
+    private int credits = 100;
+    private int betMultiplier = 1;
     private Sprite currentDeckSprite;
     private List<Card> badCards = null;
     private int handTurn = 0;
     private WinCondition gameOverCondition = WinCondition.YouLost;
-    public TextMeshProUGUI debugText;
 
     void Start() {
         Initialize();
     }
 
-    //initialize the game
+    //initialize the game / reset variables for a new hand
     void Initialize() {
+        resetBet = false;
+        gameOverWindow.SetActive(false);
+        betText.text = $"BET {betMultiplier}";
+        creditsText.text = $"CREDITS {credits}";
+        winText.text = "";
+
+        if (firstLaunch || betMultiplier > credits) {
+            ResetBetMultiplier();
+        }
+
         currentDeckSprite = deckSprites[Random.Range(0, deckSprites.Length)];
 
         badCards = null;
         gameOverCondition = WinCondition.YouLost;
-        debugText.text = "";
         handTurn = 0;
         deck = new List<Card>();
         currentHand = new List<Card>();
@@ -54,11 +80,61 @@ public class GameController : MonoBehaviour {
         PlayHand();
     }
 
+    void ResetBetMultiplier() {
+        firstLaunch = false;
+
+        foreach (Image img in betMultiplierImages) {
+            img.color = betInactiveColor;
+        }
+
+        betMultiplierImages[0].color = betSelectedColor;
+        UpdateBetMultiplier(1);
+    }
+
+    //increment the bet as long as there are enough credits
+    public void IncrementBet() {
+        if (handTurn >= 2 && !resetBet) {
+            resetBet = true;
+            ResetBetMultiplier();
+        }
+
+        if (betMultiplier+1 > credits || betMultiplier == 5) {
+            return;
+        }
+
+        betMultiplierImages[betMultiplier-1].color = betInactiveColor;
+        UpdateBetMultiplier(betMultiplier+1);
+        betMultiplierImages[betMultiplier-1].color = betSelectedColor;
+    }
+
+    public void MaxBet() {
+        int count = 1;
+
+        while (count < credits && count < 5) {
+            betMultiplierImages[count-1].color = betInactiveColor;
+            count++;
+            UpdateBetMultiplier(count);
+            betMultiplierImages[count-1].color = betSelectedColor;
+        }
+        PlayHand();
+    }
+
     public void DrawHand() {
         PlayHand();
     }
 
     void PlayHand() {
+        if (handTurn == 0) {
+            if (credits < betMultiplier) {
+                return;
+            }
+
+            AddToCredits(-betMultiplier);
+
+            betOneButton.interactable = false;
+            betMaxButton.interactable = false;
+        }
+
         //select 5 "random" cards and show them to the player
         for (int i = 0; i < gameCards.Length; i++) {
             if (handTurn == 0 || (handTurn == 1 && !gameCards[i].isHeld)) {
@@ -86,6 +162,11 @@ public class GameController : MonoBehaviour {
         StartCoroutine(WaitForCardAnalysis());
     }
 
+    void UpdateBetMultiplier(int set) {
+        betMultiplier = set;
+        betText.text = $"BET {betMultiplier}";
+    }
+
     //get the sprite for the suit
     Sprite GetSuitSprite(Card card) {
         return suitSprite[(int)card.suit];
@@ -111,17 +192,18 @@ public class GameController : MonoBehaviour {
         bool pairExists = false;
         bool threeOfAKindExists = false;
 
-        //check for a pair of jacks (if there are 1x of any 2 numbers, and both are 11 (Jack))
+        //check for a pair of jacks or better (if there are 1x of any 2 numbers, and both > 11 (Jack))
         if (currentHand.GroupBy(x => x.numericValue).Count(y => y.Count() == 2) == 1) {
             pairExists = true;
 
-            if (currentHand.GroupBy(x => x.numericValue)
-                .Where(y => y.Count() == 2)
-                .FirstOrDefault()
-                .FirstOrDefault()
-                .numericValue == 11) {
-                    
-                badCards = currentHand.Where(x => x.numericValue != 11).ToList();
+            //check for jacks or better
+            int applicableNumericValue = currentHand.GroupBy(x => x.numericValue)
+                                                    .Where(y => y.Count() == 2)
+                                                    .SelectMany(x => x)
+                                                    .FirstOrDefault().numericValue;
+
+            if (applicableNumericValue > 10) {
+                badCards = currentHand.Where(x => x.numericValue != applicableNumericValue).ToList();
 
                 gameOverCondition = WinCondition.JacksOrBetter;
             } else {
@@ -154,9 +236,12 @@ public class GameController : MonoBehaviour {
         //check for a straight (if Ace is high and 10 isn't low, set Ace to low (1), then sort the hand and check distinct cards for increasing value)
         IEnumerable<Card> sortedHand = currentHand.OrderByDescending(x => x.numericValue);
         bool setAceLow = false;
+        Card aceCard = null;
+
         if (sortedHand.Max(x => x.numericValue) == 14 && sortedHand.Min(x => x.numericValue) != 10) {
             setAceLow = true;
-            sortedHand.First().numericValue = 1;
+            aceCard = sortedHand.First();
+            aceCard.numericValue = 1;
             sortedHand = currentHand.OrderByDescending(x => x.numericValue);
         }
 
@@ -184,7 +269,7 @@ public class GameController : MonoBehaviour {
         }
 
         if (setAceLow) {
-            sortedHand.First().numericValue = 14;
+            aceCard.numericValue = 14;
         }
 
         //check for flush (only one type of suit in the hand)
@@ -222,6 +307,7 @@ public class GameController : MonoBehaviour {
             gameOverCondition = WinCondition.RoyalFlush;
         }
 
+        //hold any cards that can win (unless all can win)
         if (handTurn == 1 && badCards != null) {
             foreach (UICard card in gameCards) {
                 if (!badCards.Contains(card.referenceCard)) {
@@ -230,12 +316,10 @@ public class GameController : MonoBehaviour {
             }
         }
 
-        if (handTurn == 2 || gameOverCondition != WinCondition.YouLost) {
-            int points = (int)gameOverCondition;
-            debugText.text = $"{gameOverCondition.ToString()} - {points} points!";
-        }
-
         if (handTurn == 2) {
+            GameOver();
+
+            //dim any cards that aren't part of the winning hand
             if (badCards != null && gameOverCondition != WinCondition.YouLost) {
                 foreach (UICard card in gameCards) {
                     if (badCards.Contains(card.referenceCard)) {
@@ -244,6 +328,50 @@ public class GameController : MonoBehaviour {
                 }
             }
         }
+        
+        if (handTurn != 2 && gameOverCondition != WinCondition.YouLost) {
+            winText.text = EnumToSpacedString(gameOverCondition);
+        }
+    }
+
+    void GameOver() {
+        gameOverWindow.SetActive(true);
+
+        betOneButton.interactable = true;
+        betMaxButton.interactable = true;
+
+        int points = (int)gameOverCondition * betMultiplier;
+        if (points > 0) {
+            AddToCredits(points);
+            winText.text = $"{EnumToSpacedString(gameOverCondition)} - WIN {points}";
+            int textIndex = Array.IndexOf(Enum.GetValues(gameOverCondition.GetType()), gameOverCondition);
+            ShowWinPointText(textIndex);
+        }
+
+        if (credits == 0) {
+            PromptForReset();
+        }
+    }
+
+    void PromptForReset() {
+        resetWindow.SetActive(true);
+    }
+
+    public void ResetGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    string EnumToSpacedString(WinCondition enumCondition) {
+        return Regex.Replace(enumCondition.ToString(), "([A-Z])", " $1").Trim();
+    }
+
+    void AddToCredits(int points) {
+        credits += points;
+        creditsText.text = $"CREDITS {credits}";
+    }
+
+    void ShowWinPointText(int index) {
+        //
     }
 }
 
